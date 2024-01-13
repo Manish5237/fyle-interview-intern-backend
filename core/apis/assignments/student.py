8a79b7701e2b9c3de8b3b7297c6aef2de408e490
Manish Salunkhe <manishmaruti@iitbhilai.ac.in>
2024-01-13 12:48:02 +0530

    Added principal APIs

--- core/apis/assignments/student.py
+++ core/apis/assignments/student.py
@@ -8,6 +8,7 @@ from .schema import AssignmentSchema, AssignmentSubmitSchema
 student_assignments_resources = Blueprint('student_assignments_resources', __name__)
 
 
+
 @student_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
 def list_assignments(p):
